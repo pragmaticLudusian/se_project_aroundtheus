e134d445eee24cd04250a7ec3893cff2ca8ce5e3
pragmaticLudusian <arsen.r.m@gmail.com>
2024-08-27 21:30:07 +0300

    add javascript stage 1

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "Harbor Springs",
+    link: "https://images.unsplash.com/photo-1595521624742-47e90260edab?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Downtown Chicago",
+    link: "https://images.unsplash.com/photo-1593536768088-f01676d1330b?q=80&w=2673&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Bethlehem",
+    link: "https://images.unsplash.com/photo-1580166338999-bc03438239c0?q=80&w=2598&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Perrysburg",
+    link: "https://images.unsplash.com/photo-1685900248738-d98ec5c859aa?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Venice Beach",
+    link: "https://images.unsplash.com/photo-1504731231146-c0f65dc6a950?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    name: "Palace of Fine Arts",
+    link: "https://images.unsplash.com/photo-1573532271406-b13cb961e22e?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
