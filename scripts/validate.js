ea5d8ac4aa0a248d8bd1f6b1014cabb7d00023ac
pragmaticLudusian <arsen.r.m@gmail.com>
2024-10-12 01:19:40 +0300

    style: create form block from modal block

--- scripts/validate.js
+++ scripts/validate.js
@@ -8,9 +8,9 @@ inputProfileName.addEventListener("input", () => {
   const inputError = formProfile.querySelector(`#${inputProfileName.id}_error`);
   if (!inputProfileName.validity.valid) {
     inputError.textContent = inputProfileName.validationMessage;
-    inputError.classList.add("modal__error-message_active");
+    inputError.classList.add("form__error_active");
   } else {
-    inputError.classList.remove("modal__error-message_active");
+    inputError.classList.remove("form__error_active");
     inputError.textContent = "";
   }
 });
