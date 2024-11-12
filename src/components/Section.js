62c8e217143a0432d9a9d4dc0afe943d0db86edf
pragmaticLudusian <arsen.r.m@gmail.com>
2024-11-12 02:46:53 +0200

    bugfix: fix definition of selector

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,16 +3,17 @@ export default class Section {
   constructor({ items, renderer }, selector) {
     this._items = items; // array of objects
     this._renderer = renderer; // custom-tailored func for classes
-    this._selector = document.querySelector(selector); // class/id selector
+    this._selector = selector; // class/id selector (typeof string)
   }
 
   renderItems() {
+    this._container = document.querySelector(this._selector);
     this._items.forEach((item) => {
       this._renderer(item); // pass item out of array as arg to the function body inside index.js
     });
   }
 
   addItem(element, method = "append") {
-    this._selector[method](element); // add elem to selected container
+    this._container[method](element); // add elem to selected container
   }
 }
